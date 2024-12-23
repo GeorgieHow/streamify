d4ab5573dc42e415d510c05108bb4c9402c56772
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-12-23 09:18:07 +0000

    Created a navbar and searchbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
